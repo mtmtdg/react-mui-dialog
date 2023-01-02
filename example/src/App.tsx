4b2e74f2169ec2aaffb2be0092a9716c2d9b4e97
choug <chougousui@gmail.com>
2023-01-02 23:06:10 +0900

    add example

---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.scss';
+import ModalTest from './ModalTest/ModalTest';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <ModalTest />
+    </div>
+  );
+}
+
+export default App;
